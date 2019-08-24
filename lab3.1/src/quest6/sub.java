33d5af64e5b03cc8b4ad5ba9f2de62f2a0648297
Brown <cibrown@labs.neit.edu>
2019-08-24 13:38:16 -0400

    after quest 6

---
+++ lab3.1/src/quest6/sub.java
@@ -0,0 +1,11 @@
+package quest6;
+
+import java.util.ArrayList;
+
+	public interface sub {
+		public void addPeep( peep x);
+		public void delPeep( peep x);
+		public void shout();
+			
+	}
+
---
+++ lab3.1/src/quest6/thingy.java
@@ -0,0 +1,5 @@
+package quest6;
+public interface thingy {
+
+	public void rollCall();
+}
